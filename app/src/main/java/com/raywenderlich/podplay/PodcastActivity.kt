e21b12bff2c10960c13f62753fe7f1f0cffb9329
Kausi Ahmed <KausiAhmed@users.noreply.github.com>
2023-06-13 18:14:10 -0700

    Chapter 20 - Completed | Chapter 21 Menu Partial Completed

---
+++ app/src/main/java/com/raywenderlich/podplay/PodcastActivity.kt
@@ -0,0 +1,43 @@
+package com.raywenderlich.podplay
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.ItunesService
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import androidx.appcompat.app.AppCompatActivity
+
+class PodcastActivity : AppCompatActivity() {
+    val TAG = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm("Android Developer")
+            Log.i(TAG, "Results = ${results.body()}")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
+    }
+
+}
